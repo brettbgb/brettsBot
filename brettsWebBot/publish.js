7a30e40fb9493b300ad26ee8c6d67e6c0b6eb476
Brett Bisyak <sbbisyak@live.com>
2018-11-05 16:58:26 -0800

    new bot for MS Interview

---
+++ brettsWebBot/publish.js
@@ -0,0 +1,52 @@
+var zipFolder = require('zip-folder');
+var path = require('path');
+var fs = require('fs');
+var request = require('request');
+
+var rootFolder = path.resolve('.');
+var zipPath = path.resolve(rootFolder, '../brettswebbot.zip');
+var kuduApi = 'https://brettswebbot.scm.azurewebsites.net/api/zip/site/wwwroot';
+var userName = '$brettswebbot';
+var password = 'i5FRaLL6aolpLxdhvuWsyom7otbGcmeAotuCi9hdDDmgqqfflt34p03W7AbG';
+
+function uploadZip(callback) {
+  fs.createReadStream(zipPath).pipe(request.put(kuduApi, {
+    auth: {
+      username: userName,
+      password: password,
+      sendImmediately: true
+    },
+    headers: {
+      "Content-Type": "applicaton/zip"
+    }
+  }))
+  .on('response', function(resp){
+    if (resp.statusCode >= 200 && resp.statusCode < 300) {
+      fs.unlink(zipPath);
+      callback(null);
+    } else if (resp.statusCode >= 400) {
+      callback(resp);
+    }
+  })
+  .on('error', function(err) {
+    callback(err)
+  });
+}
+
+function publish(callback) {
+  zipFolder(rootFolder, zipPath, function(err) {
+    if (!err) {
+      uploadZip(callback);
+    } else {
+      callback(err);
+    }
+  })
+}
+
+publish(function(err) {
+  if (!err) {
+    console.log('brettswebbot publish');
+  } else {
+    console.error('failed to publish brettswebbot', err);
+  }
+});
